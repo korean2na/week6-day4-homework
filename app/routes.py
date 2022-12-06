4382194cab2c92825fb8e32de3eaddb4c07dd629
John Han <sirjohn146@gmail.com>
2022-12-05 20:45:39 -0600

    First commit

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
